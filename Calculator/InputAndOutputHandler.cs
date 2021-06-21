328807185bdaa44c2e82105fe2c31103c4ddf711
Daniella Noren <daniellanoren@live.se>
2021-06-21 20:38:24 +0200

    Create InputAndOutputHandler class

---
+++ Calculator/InputAndOutputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class InputAndOutputHandler 
+    {
+        public string HandleInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleOutput(string outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+
+        
+    }
+}
