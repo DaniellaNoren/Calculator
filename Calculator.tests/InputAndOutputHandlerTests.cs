419a5acfcd771019e8244d1f2cfb78a4d0177d5c
Daniella Noren <daniellanoren@live.se>
2021-06-23 09:08:48 +0200

    Test regex

---
+++ Calculator.tests/InputAndOutputHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculator.tests
+{
+    public class InputAndOutputHandlerTests
+    {
+        private readonly InputAndOutputHandler sut;
+        public InputAndOutputHandlerTests()
+        {
+            sut = new InputAndOutputHandler();
+        }
+
+        [Theory]
+        [InlineData("6", true)]
+        [InlineData("7,4", true)]
+        [InlineData("0.0002", false)]
+        [InlineData("0,0002", true)]
+        [InlineData("G", false)]
+        [InlineData("7.", false)]
+        [InlineData("-", false)]
+        [InlineData(" ", false)]
+        public void RegexTest(string input, bool expected)
+        {
+            Assert.Equal(expected, sut.InputIsValidNumber(input));
+        }
+    }
+}
